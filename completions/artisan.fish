bcbe095cdc366756047e23b4a48b91d9e070375e
Adriaan Zonnenberg <amz@adriaan.xyz>
2017-01-22 00:08:17 +0100

    Add command completion for `php artisan help`

--- completions/artisan.fish
+++ completions/artisan.fish
@@ -5,7 +5,17 @@ function __fish_artisan_needs_command
   test (count (commandline -opc)) -eq 1
 end
 
-function __fish_artisan_complete --description "Completion for Laravel's artisan command"
+function __fish_artisan_using_command
+  test -f artisan
+  or return 1
+
+  set cmd (commandline -opc)
+
+  test (count $cmd) -gt 1
+  and test $argv[1] = $cmd[2]
+end
+
+function __fish_artisan_commands_with_descriptions
   php artisan --no-ansi ^ /dev/null \
     | sed '1,/Available commands/d' \
     | grep -E '^  ' \
@@ -13,4 +23,9 @@ function __fish_artisan_complete --description "Completion for Laravel's artisan
     | sed 's/\s\+/\t/'
 end
 
-complete -c artisan -f -n '__fish_artisan_needs_command' -a '(__fish_artisan_complete)'
+function __fish_artisan_commands
+  __fish_artisan_commands_with_descriptions | cut -f 1
+end
+
+complete -c artisan -f -n '__fish_artisan_needs_command' -a '(__fish_artisan_commands_with_descriptions)'
+complete -c artisan -f -n '__fish_artisan_using_command help' -a '(__fish_artisan_commands)'
