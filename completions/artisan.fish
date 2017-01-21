a9cb591fc8260f6e981548d3cb950f65e3cbf10e
Adriaan Zonnenberg <amz@adriaan.xyz>
2017-01-21 23:41:18 +0100

    Only complete when there are no arguments yet

--- completions/artisan.fish
+++ completions/artisan.fish
@@ -1,9 +1,16 @@
-function __artisan_complete --description "Completion for Laravel's artisan command"
-  artisan --no-ansi ^ /dev/null \
+function __fish_artisan_needs_command
+  test -f artisan
+  or return 1
+
+  test (count (commandline -opc)) -eq 1
+end
+
+function __fish_artisan_complete --description "Completion for Laravel's artisan command"
+  php artisan --no-ansi ^ /dev/null \
     | sed '1,/Available commands/d' \
     | grep -E '^  ' \
     | sed 's/^  //' \
     | sed 's/\s\+/\t/'
 end
 
-complete -c artisan -f -n 'test -f artisan' -a '(__artisan_complete)'
+complete -c artisan -f -n '__fish_artisan_needs_command' -a '(__fish_artisan_complete)'
