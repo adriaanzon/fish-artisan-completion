85ab5f0c96c442fe3b3f3fa846b9ff62f0a3df78
Adriaan Zonnenberg <amz@adriaan.xyz>
2017-02-06 22:24:28 +0100

    Use fish's own helper functions to check for subcommands

--- completions/artisan.fish
+++ completions/artisan.fish
@@ -1,20 +1,3 @@
-function __fish_artisan_needs_command
-  test -f artisan
-  or return 1
-
-  test (count (commandline -opc)) -eq 1
-end
-
-function __fish_artisan_using_command
-  test -f artisan
-  or return 1
-
-  set cmd (commandline -opc)
-
-  test (count $cmd) -gt 1
-  and test $argv[1] = $cmd[2]
-end
-
 function __fish_artisan_commands_with_descriptions
   php artisan --no-ansi ^ /dev/null \
     | sed '1,/Available commands/d' \
@@ -27,5 +10,5 @@ function __fish_artisan_commands
   __fish_artisan_commands_with_descriptions | cut -f 1
 end
 
-complete -c artisan -f -n '__fish_artisan_needs_command' -a '(__fish_artisan_commands_with_descriptions)'
-complete -c artisan -f -n '__fish_artisan_using_command help' -a '(__fish_artisan_commands)'
+complete -c artisan -f -n 'test -f artisan; and __fish_use_subcommand' -a '(__fish_artisan_commands_with_descriptions)'
+complete -c artisan -f -n 'test -f artisan; and __fish_seen_subcommand_from help' -a '(__fish_artisan_commands)'
