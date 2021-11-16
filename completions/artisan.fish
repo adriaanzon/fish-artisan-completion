8e8d726b3862fcb972abb652fb8c1a9fb9207a64
Adriaan Zonnenberg <adriaanzonn@gmail.com>
2021-11-16 23:45:57 +0100

    Handle non-zero exit code
    Cancel executing when an error occurs, otherwise error messages may end up in the completion result.

--- completions/artisan.fish
+++ completions/artisan.fish
@@ -1,9 +1,12 @@
 function __fish_artisan_commands_with_descriptions
-  php artisan list --raw 2> /dev/null | grep -vE '^ ' | string replace -r '\s+' '\t'
+    begin
+        php artisan list --raw 2>/dev/null
+        or return
+    end | grep -vE '^ ' | string replace -r '\s+' '\t'
 end
 
 function __fish_artisan_commands
-  __fish_artisan_commands_with_descriptions | cut -f 1
+    __fish_artisan_commands_with_descriptions | cut -f 1
 end
 
 complete -c artisan -f -n 'test -f artisan; and __fish_use_subcommand' -a '(__fish_artisan_commands_with_descriptions)'
